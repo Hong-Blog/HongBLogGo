3ae4ab18715810f9d787feeba1c9368dd2f30d58
hong <lun3322@126.com>
2020-10-16 16:25:55 +0800

    添加docker数据库环境

--- usercenter/db/db.go
+++ usercenter/db/db.go
@@ -9,8 +9,7 @@ import (
 var Db *sqlx.DB
 
 func init() {
-	//db, err := sql.Open("mysql", "root:123qwe@tcp(127.0.0.1:3306)/oneblog")
-	db, err := sqlx.Connect("mysql", "root:123qwe@tcp(127.0.0.1:3306)/oneblog?parseTime=true")
+	db, err := sqlx.Connect("mysql", "root:123qwe@tcp(127.0.0.1:3306)/hongblog?parseTime=true")
 	if err != nil {
 		log.Panicln("db err: ", err.Error())
 	}
