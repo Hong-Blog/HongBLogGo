d6a0972146acafa6dfab10f6974963c0c48f932d
hong <lun3322@126.com>
2020-11-18 17:33:26 +0800

    添加 docker 支持

--- usercenter/db/db.go
+++ usercenter/db/db.go
@@ -4,12 +4,17 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"log"
+	"os"
 )
 
 var Db *sqlx.DB
 
 func init() {
-	db, err := sqlx.Connect("mysql", "root:123qwe@tcp(127.0.0.1:3306)/hongblog?parseTime=true")
+	dataSource := os.Getenv("DATA_SOURCE")
+	if dataSource == "" {
+		dataSource = "root:123qwe@tcp(127.0.0.1:3306)/hongblog?parseTime=true"
+	}
+	db, err := sqlx.Connect("mysql", dataSource)
 	if err != nil {
 		log.Panicln("db err: ", err.Error())
 	}
