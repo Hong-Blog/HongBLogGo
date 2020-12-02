3d0e49babb3cb7871d778779460030eab5eaa46a
hong <lun3322@126.com>
2020-12-02 10:51:00 +0800

    重构

--- usercenter/models/sysUser/SysUser.go
+++ usercenter/models/sysUser/SysUser.go
@@ -2,79 +2,10 @@ package sysUser
 
 import (
 	"errors"
-	"github.com/guregu/null"
 	"log"
 	"usercenter/db"
-	"usercenter/models"
 	"usercenter/utils"
 )
-
-type SysUser struct {
-	Id            int         `json:"id"`
-	Username      null.String `json:"username" swaggertype:"string"`
-	Password      null.String `json:"password" swaggertype:"string"`                             // 登录密码
-	Nickname      null.String `json:"nickname" swaggertype:"string"`                             // 昵称
-	Mobile        null.String `json:"mobile" swaggertype:"string"`                               // 手机号
-	Email         null.String `json:"email" swaggertype:"string"`                                // 邮箱地址
-	Qq            null.String `json:"qq" swaggertype:"string"`                                   // QQ
-	Birthday      null.Time   `json:"birthday" swaggertype:"string"`                             // 生日
-	Gender        null.Int    `json:"gender" swaggertype:"integer"`                              // 性别
-	Avatar        null.String `json:"avatar" swaggertype:"string"`                               // 头像地址
-	UserType      null.String `json:"user_type" db:"user_type" swaggertype:"string"`             // 超级管理员、管理员、普通用户
-	Company       null.String `json:"company" swaggertype:"string"`                              // 公司
-	Blog          null.String `json:"blog" swaggertype:"string"`                                 // 个人博客地址
-	Location      null.String `json:"location" swaggertype:"string"`                             // 地址
-	Source        null.String `json:"source" swaggertype:"string"`                               // 用户来源
-	Uuid          null.String `json:"uuid" swaggertype:"string"`                                 // 用户唯一表示(第三方网站)
-	Privacy       null.Int    `json:"privacy" swaggertype:"integer"`                             // 隐私（1：公开，0：不公开）
-	Notification  null.Int    `json:"notification" swaggertype:"integer"`                        // 通知：(1：通知显示消息详情，2：通知不显示详情)
-	Score         null.Int    `json:"score" swaggertype:"integer"`                               // 金币值
-	Experience    null.Int    `json:"experience" swaggertype:"integer"`                          // 经验值
-	RegIp         null.String `json:"reg_ip" db:"reg_ip" swaggertype:"string"`                   // 注册IP
-	LastLoginIp   null.String `json:"last_login_ip" db:"last_login_ip" swaggertype:"string"`     // 最近登录IP
-	LastLoginTime null.Time   `json:"last_login_time" db:"last_login_time" swaggertype:"string"` // 最近登录时间
-	LoginCount    null.Int    `json:"login_count" db:"login_count" swaggertype:"integer"`        // 登录次数
-	Remark        null.String `json:"remark" swaggertype:"string"`                               // 用户备注
-	Status        null.Int    `json:"status" swaggertype:"integer"`                              // 用户状态
-	CreateTime    null.Time   `json:"create_time" db:"create_time" swaggertype:"string"`         // 注册时间
-	UpdateTime    null.Time   `json:"update_time" db:"update_time" swaggertype:"string"`         // 更新时间
-}
-
-type GetAllUserRequest struct {
-	models.PagedRequest
-	KeyWord string `json:"key_word"`
-}
-
-type UpdateUserRequest struct {
-	Id       int    `uri:"id"`
-	Nickname string `json:"nickname"` // 昵称
-	Mobile   string `json:"mobile"`   // 手机号
-	Email    string `json:"email"`    // 邮箱地址
-	Qq       string `json:"qq"`       // QQ
-}
-
-type AddUserRequest struct {
-	Username string `json:"username" binding:"required" display:"用户名"`
-	Password string `json:"password" binding:"required" display:"密码"` // 登录密码
-	Nickname string `json:"nickname" binding:"required" display:"昵称"` // 昵称
-	Mobile   string `json:"mobile"`                                   // 手机号
-	Email    string `json:"email" binding:"omitempty,email"`          // 邮箱地址
-	Qq       string `json:"qq"`                                       // QQ
-	RegIp    string `json:"reg_ip"`                                   // 注册IP
-}
-
-type UpdatePasswordByIdRequest struct {
-	Id               int    `json:"id"`
-	Username         string `json:"username"`
-	Password         string `json:"password"`           // 当前密码
-	NewPassword      string `json:"new_password"`       // 新密码
-	NewPasswordAgain string `json:"new_password_again"` // 新密码
-}
-
-type UpdateUserRoleRequest struct {
-	Id     int `json:"id"`
-	RoleId int `json:"role_id"`
-}
 
 func GetAllUser(request GetAllUserRequest) (list []SysUser, count int) {
 	strSql := `
