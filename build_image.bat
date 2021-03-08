fb1beb6ab59f7c51e99ca7cf1325adc724fb048d
hong <lun3322@126.com>
2021-03-08 17:39:39 +0800

    新增 docker脚本

---
+++ build_image.bat
@@ -0,0 +1,2 @@
+docker build -t hong-post:0.1 -f post.Dockerfile .
+docker build -t hong-user-center:0.1 -f user.Dockerfile .
---
+++ push_image.bat
@@ -0,0 +1,7 @@
+docker login --username=lun3322@aliyun.com registry.cn-beijing.aliyuncs.com
+
+docker tag hong-post:0.1 registry.cn-beijing.aliyuncs.com/lun3322/hong-post:0.1
+docker tag hong-user-center:0.1 registry.cn-beijing.aliyuncs.com/lun3322/hong-user-center:0.1
+
+docker push registry.cn-beijing.aliyuncs.com/lun3322/hong-post:0.1
+docker push registry.cn-beijing.aliyuncs.com/lun3322/hong-user-center:0.1
